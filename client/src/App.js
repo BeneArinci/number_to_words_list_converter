373eabde6186cb3b0873ebadd20871c8d79fc7e3
BeneArinci <bene@tray.io>
2021-06-06 18:57:16 +0100

    Basic form + set input state.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,24 @@
+import { useState } from 'react';
+
 const App = () => {
+
+	const [ getInput, setGetInput ] = useState(null)
+
+	const onInputChange = (event) => {
+		setGetInput(event.target.value)
+		console.log(getInput)
+	}
+
   return (
-    <h1> HELLOOOOOOO! </h1>
+		<center>
+			<form>
+				<label>
+						Enter your number:
+				<input type="text" onChange={onInputChange}/>
+				</label>
+				<input type="submit" value="Convert"/>
+			</form>
+		</center>
   );
 }
 
