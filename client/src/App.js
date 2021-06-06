7cc905f0c6099bc9d6730a441104db29f53f437d
BeneArinci <bene@tray.io>
2021-06-06 21:11:14 +0100

    Fetch data from api based on passed input.

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,18 @@ import { useState } from 'react';
 const App = () => {
 
 	const [ getInput, setGetInput ] = useState(null)
+	const [ getWordsList, setGetWordsList ] = useState([])
 
 	const onInputChange = (event) => {
 		setGetInput(event.target.value)
-		console.log(getInput)
+	}
+
+	const convertNumber = async (event) => {
+		event.preventDefault()
+		const apiResponse = await fetch(`http://localhost:8000/api/wordsList?number=${getInput}`)
+		const { data: wordsList } = await apiResponse.json()
+		setGetWordsList(wordsList)
+		console.log(wordsList)
 	}
 
   return (
@@ -16,7 +24,7 @@ const App = () => {
 						Enter your number:
 				<input type="text" onChange={onInputChange}/>
 				</label>
-				<input type="submit" value="Convert"/>
+				<input type="submit" value="Convert" onClick={(e)=>{convertNumber(e)}}/>
 			</form>
 		</center>
   );
