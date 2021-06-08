986492d08993720c6664bf462ba0662506970761
BeneArinci <bene@tray.io>
2021-06-09 00:14:42 +0100

    Jest setup.

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     browser: true,
     commonjs: true,
     es2021: true,
+    'jest/globals': true,
   },
   extends: [
     'airbnb-base',
@@ -11,5 +12,11 @@ module.exports = {
     ecmaVersion: 12,
   },
   rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
   },
+  plugins: ['jest'],
 };
